a61db1dee1914699513527b9f5df2cff3975ad9d
Stephane Epardaud <stef@epardaud.fr>
2013-03-22 13:37:10 +0100

    Ex4Java: more similar to Ceylon

--- source/ceylon99/Ex04.java
+++ source/ceylon99/Ex04.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 public class Ex04 {
 	public static void main(String[] args) {
-		List<? extends Object> items = Arrays.asList(1, 2, 3, 3.0, "a");
+		List<? extends Object> items = Arrays.asList(1, 2, 3, 3.0);
 		for(Object i : items){
-			System.out.println(i);
+			System.out.println(((Number)i).floatValue());
+			System.out.println(((Comparable<Number>)i).compareTo((Number)i));
 		}
 	}
 }
